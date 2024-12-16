22e3d7e880dbfa23defa5258782ae3e623442ac0
abhilekhx <lekh7adhikari@gmail.com>
2024-12-16 11:26:06 -0500

    Navbar for GS page

---
+++ src/components/Navbar.js
@@ -0,0 +1,63 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import logo from "../logo2.png";
+// REACT FONT AWESOME IMPORTS
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light">
+      <div className="container">
+        <a className="navbar-brand" href="#">
+          <img className="logo" src={logo} alt="logo"></img>
+          <span className="brandName">Gentle Support</span>
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          {/* <span className="navbar-toggler-icon"></span> */}
+          <FontAwesomeIcon icon={faBars} style={{ color: "#003333" }} />
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                Services
+              </a>
+            </li>
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                Resources
+              </a>
+            </li>
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                About Us
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Contact Us
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
