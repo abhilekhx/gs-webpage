159e2228daf29902307b1f3f131a6508626a86bc
abhilekhx <lekh7adhikari@gmail.com>
2024-12-17 23:48:12 -0500

    Services complete

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import AboutUs from "./components/AboutUs";
+import Services from "./components/Services";
 
 function App() {
   return (
     <>
       <Navbar />
       <Header />
+      <Services />
       <AboutUs />
     </>
   );
